2ab8e887fa9fac29852c5a3cfe9c725b59b2b5b2
castetus <kboikov@mail.ru>
2020-10-07 13:10:59 +0300

    initial commit

---
+++ upload/admin/language/en-gb/extension/module/related.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']     = 'Related Products';
+
+// Text
+$_['text_extension']    = 'Extensions';
+$_['text_success']      = 'Success: You have modified Related Products!';
+$_['text_edit']         = 'Edit Related Products';
+
+// Entry
+$_['entry_name']        = 'Module Name';
+$_['entry_status']      = 'Status';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify Related Products!';
+$_['error_name']        = 'Module Name must be between 3 and 64 characters!';
+
+
